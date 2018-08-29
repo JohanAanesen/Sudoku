59baf9eee56cfc1a15479d5f3880ad7a04daa76b
JohanAanesen <johanaan@stud.ntnu.no>
2018-08-29 14:20:29 +0200

    added grid and button

--- src/no/ntnu/imt3281/sudoku/Sudoku.java
+++ src/no/ntnu/imt3281/sudoku/Sudoku.java
@@ -12,7 +12,7 @@ public class Sudoku extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sudoku.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
